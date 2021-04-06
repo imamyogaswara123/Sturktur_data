f7704960ad3e27de5d050f0c2a4f057a35c4e230
ranimarliana <82036794+ranimarliana@users.noreply.github.com>
2021-04-06 20:22:23 +0700

    Struktur data Imam Yogaswara

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "jam.h"
+
+int main() {
+    tJam jam;
+
+    jam.setJam();
+    jam.setMenit();
+    jam.setDetik();
+    jam.cetakJam();
+
+
+    //cout << "Jam sekarang " << jam.hh << ":" << jam.mm << ":" << jam.ss << endl;
+    //cout << "Hello world!" << endl;
+    return 0;
+}
